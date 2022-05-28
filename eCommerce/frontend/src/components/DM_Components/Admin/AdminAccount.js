12dc452f33b64102bdfbff461310088114ab880e
dhananjayamadhusankha <mdhananjaya04@gmail.com>
2022-05-29 00:14:43 +0530

    Add some files

---
+++ eCommerce/frontend/src/components/DM_Components/Admin/AdminAccount.js
@@ -0,0 +1,50 @@
+// import React, { Component } from 'react'
+
+// // import img1 from '../img/seller_account/img_01.svg';
+// // import img2 from '../img/seller_account/img_02.svg';
+// // import img3 from '../img/seller_account/img_03.svg';
+
+
+// export default class AdminAccount extends Component {
+//     render() {
+//         return (
+//             <div>
+//                  {/* <!-- Seller Account Intro Starts --> */}
+//   <div className="container-fluid mb-5" >
+//     <div className="row">
+//       <div className="col-md-12 p-5 text-white text-center" style="background-color: #008577;">
+//         <h1>"Since last 6 years my friends wealth increased by 3000%, thanks to selling of BSNL, LIC, AIR India & etc on Amazon."</h1>
+//         <p>Narendra Modi, <em>PMO India</em> (Amazon Seller)</p>
+//         <a href="your_seller_account.html" className="btn btn-warning rounded-pill">Start selling</a>
+//         <p>It only takes 15 minutes to setup your account</p>
+//       </div>
+//     </div>
+
+//     <div className="row">
+//       <div className="col-md-12 pl-5 pr-5 pt-3 pb-2">
+//         <h2>Why sell on Amazon?</h2>
+//       </div>
+//     </div>
+    
+//     <div className="row">
+//       <div className="col-md-4 p-5">
+//         <img src={img1} className="mb-2" height="100" alt=""/>
+//         <h3>E-Commerce: Your next big sales channel</h3>
+//         <div>Start selling on Amazon - without building a website. Register now to start selling on Amazon.in.</div>
+//       </div>
+//       <div className="col-md-4 p-5">
+//         <img src={img2} className="mb-2" height="100" alt=""/>
+//         <h3>Reach crores of customers</h3>
+//         <div>Sell to crores of engaged customer visiting Amazon.in on desktop and through our mobile app.</div>
+//       </div>
+//       <div className="col-md-4 p-5">
+//         <img src={img3} className="mb-2" height="100" alt=""/>
+//         <h3>Receive timely payments</h3>
+//         <div >Amazon ensures your payments for your delivered orders are deposited directly in your bank account every 7 days.</div>
+//       </div>
+//     </div>
+//   </div>
+//             </div>
+//         ) 
+//     }
+// }
